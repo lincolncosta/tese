06ab4ea2b75474b066504b45e2e6e2aa9e4fabfa
tekpixo <lincoln.mcosta@gmail.com>
2023-10-20 19:57:33 -0300

    feat: formata dataset com eventos a cada 1 min e inicia execução dos notebooks

--- crawlers/format-time-unified-dataset.py
+++ crawlers/format-time-unified-dataset.py
@@ -1,29 +1,112 @@
 import pandas as pd
-import numpy as np
-
-def qtd_eventos(integer):
-    qtdNulos = integer / 2
-    return int(45 - qtdNulos)
+from tqdm import tqdm
+import csv
 
 df = pd.read_csv('../data/crawler/unified-events-statistics.csv')
+ordem_colunas = ['BLUE:first_blood',
+    'BLUE:dragon',
+    'BLUE:herald',
+    'BLUE:first_tower_top',
+    'BLUE:first_tower_mid',
+    'BLUE:first_tower_bot',
+    'BLUE:second_tower_top',
+    'BLUE:second_tower_mid',
+    'BLUE:second_tower_bot',
+    'BLUE:third_tower_top',
+    'BLUE:third_tower_mid',
+    'BLUE:third_tower_bot',
+    'BLUE:inhibitor_top',
+    'BLUE:inhibitor_mid',
+    'BLUE:inhibitor_bot',
+    'BLUE:baron',
+    'BLUE:elder_dragon',
+    'BLUE:nexus_tower',
+    'BLUE:nexus',
+    'RED:first_blood',
+    'RED:dragon',
+    'RED:herald',
+    'RED:first_tower_top',
+    'RED:first_tower_mid',
+    'RED:first_tower_bot',
+    'RED:second_tower_top',
+    'RED:second_tower_mid',
+    'RED:second_tower_bot',
+    'RED:third_tower_top',
+    'RED:third_tower_mid',
+    'RED:third_tower_bot',
+    'RED:inhibitor_top',
+    'RED:inhibitor_mid',
+    'RED:inhibitor_bot',
+    'RED:baron',
+    'RED:elder_dragon',
+    'RED:nexus_tower',
+    'RED:nexus']
 
-# Formatando colunas
-qtd_eventos = list(map(qtd_eventos, df.isnull().sum(axis=1).tolist()))
-df['qtd_eventos'] = qtd_eventos
-
-for index, row in df.iterrows():
-    qtdEventos = row['qtd_eventos']
-    duracao = row['event{}time'.format(qtdEventos)]    
-    df.at[index, 'duracao_min'] = int(duracao.split(':')[0])
-
-    for i in range(1, 46):
-        if isinstance(row['event{}time'.format(i)], str):
-            df.at[index, 'event{}time'.format(i)] = int(row['event{}time'.format(i)].split(':')[0])
+for index, row in tqdm(df.iterrows(), total=len(df)):
+  
+    result = row['result']
     
+    # Busca os eventos da partida a cada 1 minuto até os 60 minutos
+    for time_considered in range(0, 61, 1):
+        eventos = []
+        contagem = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        duracao_min = row['duracao_min']
 
-df = df.astype({'duracao_min':'int'})
-convert_dict = {'event1time': 'Int64','event2time': 'Int64','event3time': 'Int64','event4time': 'Int64','event5time': 'Int64','event6time': 'Int64','event7time': 'Int64','event8time': 'Int64','event9time': 'Int64','event10time': 'Int64','event11time': 'Int64','event12time': 'Int64','event13time': 'Int64','event14time': 'Int64','event15time': 'Int64','event16time': 'Int64','event17time': 'Int64','event18time': 'Int64','event19time': 'Int64','event20time': 'Int64','event21time': 'Int64','event22time': 'Int64','event23time': 'Int64','event24time': 'Int64','event25time': 'Int64','event26time': 'Int64','event27time': 'Int64','event28time': 'Int64','event29time': 'Int64','event30time': 'Int64','event31time': 'Int64','event32time': 'Int64','event33time': 'Int64','event34time': 'Int64','event35time': 'Int64','event36time': 'Int64','event37time': 'Int64','event38time': 'Int64','event39time': 'Int64','event40time': 'Int64','event41time': 'Int64','event42time': 'Int64','event43time': 'Int64','event44time': 'Int64','event45time': 'Int64'} 
-df = df.astype(convert_dict, errors='ignore')
+        # Se a partida já estiver finalizada nessa iteração, vai para a próxima partida
+        if time_considered > duracao_min:
+                break
 
+        for i in range(1, 46):
+            evento = f'event{i}'
+            tempo = f'event{i}time'
 
-df.to_csv('../data/crawler/unified-events-statistics.csv', index=False)
+            # Verificar se o evento aconteceu antes de "time_considered" minutos
+            if pd.notna(row[tempo]) and row[tempo] <= time_considered:
+                eventos.append(row[evento].replace(' ', ''))
+
+                # Criar um DataFrame com base na lista de colunas
+                df_contagem = pd.DataFrame({'coluna': eventos})
+
+                # Contar as ocorrências de cada coluna
+                contagem = df_contagem['coluna'].value_counts().reindex(ordem_colunas)
+                contagem = contagem.fillna(0).astype(int).to_list()
+
+        golId = row['golId']
+        time = time_considered        
+        blueTopGP = row['blueTopGP']
+        blueTopWR = row['blueTopWR']
+        blueTopKDA = row['blueTopKDA']
+        blueJungleGP = row['blueJungleGP']
+        blueJungleWR = row['blueJungleWR']
+        blueJungleKDA = row['blueJungleKDA']
+        blueMidGP = row['blueMidGP']
+        blueMidWR = row['blueMidWR']
+        blueMidKDA = row['blueMidKDA']
+        blueADCGP = row['blueADCGP']
+        blueADCWR = row['blueADCWR']
+        blueADCKDA = row['blueADCKDA']
+        blueSupportGP = row['blueSupportGP']
+        blueSupportWR = row['blueSupportWR']
+        blueSupportKDA = row['blueSupportKDA']
+        redTopGP = row['redTopGP']
+        redTopWR = row['redTopWR']
+        redTopKDA = row['redTopKDA']
+        redJungleGP = row['redJungleGP']
+        redJungleWR = row['redJungleWR']
+        redJungleKDA = row['redJungleKDA']
+        redMidGP = row['redMidGP']
+        redMidWR = row['redMidWR']
+        redMidKDA = row['redMidKDA']
+        redAdcGP = row['redAdcGP']
+        redAdcWR = row['redAdcWR']
+        redAdcKDA = row['redAdcKDA']
+        redSupportGP = row['redSupportGP']
+        redSupportWR = row['redSupportWR']
+        redSupportKDA = row['redSupportKDA']
+        
+        features = [golId,time,result,blueTopGP,blueTopWR,blueTopKDA,blueJungleGP,blueJungleWR,blueJungleKDA,blueMidGP,blueMidWR,blueMidKDA,blueADCGP,blueADCWR,blueADCKDA,blueSupportGP,blueSupportWR,blueSupportKDA,redTopGP,redTopWR,redTopKDA,redJungleGP,redJungleWR,redJungleKDA,redMidGP,redMidWR,redMidKDA,redAdcGP,redAdcWR,redAdcKDA,redSupportGP,redSupportWR,redSupportKDA]
+        features.extend(contagem)
+
+        with open('../data/crawler/unified-events-time-statistics.csv', mode='a', newline="") as dataset:            
+            datasetWriter = csv.writer(dataset, delimiter=',')
+            datasetWriter.writerow(features)
